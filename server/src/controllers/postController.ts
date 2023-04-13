59cb3077d582355a9e3ade559018808f2b88baa2
EdwinTJ <edwin16x@gmail.com>
2023-04-12 18:46:28 -0600

    Add CRUD operations to post

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -1,38 +1,97 @@
 import { Example, PrismaClient } from "@prisma/client";
+import { cloudinary } from "../util/cloudinary";
+
 const prisma = new PrismaClient();
 
 export const getAllPosts = async (req: any, res: any, next: any) => {
-  const all = await prisma.example.findMany();
-  res.json(all);
+  const post = await prisma.post.findMany({
+    include: {
+      comments: true,
+      image: true,
+    },
+  });
+  res.json({
+    success: true,
+    post: post,
+  });
 };
 
 // Create a new post
-export const newPost = async (
-  req: { body: { name: any } },
-  res: { json: (arg0: Example) => void },
-  next: any
-) => {
-  const { name } = req.body;
-  const created = await prisma.example.create({
-    data: {
-      name,
-    },
-  });
-  res.json(created);
+export const newPost = async (req: any, res: any) => {
+  const { title, image } = req.body;
+
+  if (image === null || image === undefined) {
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+      },
+    });
+    res.json({
+      success: true,
+      newPost,
+    });
+  } else {
+    //TODO: Upload image to cloudinary
+    // 1. Upload image to cloudinary
+    // 2. Get the image url
+    // 3. Get the public url and public id from cloudinary
+    // 4. Save the public url and public id to the database
+    const publicId = image + "publicId";
+    const newPost = await prisma.post.create({
+      data: {
+        title,
+        image: {
+          create: {
+            url: image,
+            public_id: publicId,
+          },
+        },
+      },
+    });
+    res.json({
+      success: true,
+      newPost,
+    });
+  }
 };
 
 // Update a post
 export const updatePost = async (req: any, res: any) => {
   const { id } = req.params;
-  const { name } = req.body;
-  console.log("id" + id);
-  const updated = await prisma.example.update({
+  const { title } = req.body;
+  const updated = await prisma.post.update({
     where: {
       id: id,
     },
     data: {
-      name,
+      title,
     },
   });
-  res.json(updated);
+  res.json({
+    success: true,
+    updated,
+  });
+};
+
+// Delete a post
+export const deletePost = async (req: any, res: any) => {
+  const { id } = req.params;
+  try {
+    const deleted = await prisma.post.delete({
+      where: { id: id },
+    });
+
+    Promise.all([deleted])
+      .then(() => {
+        res.status(200).json({
+          success: true,
+          message: "Post deleted successfully",
+        });
+      })
+      .catch((error) => {
+        console.log("Error inside Promise:" + error);
+      });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- server/src/routes/postRoutes.ts
+++ server/src/routes/postRoutes.ts
@@ -3,9 +3,12 @@ import {
   getAllPosts,
   newPost,
   updatePost,
+  deletePost,
 } from "../controllers/postController";
 const router = Router();
 
 router.get("/", getAllPosts);
-
+router.post("/create", newPost);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
 export { router as postRoutes };
