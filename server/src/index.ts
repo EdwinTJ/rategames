1e229b72195cf33b791dc08bdbde7f5a0f126c45
EdwinTJ <edwin16x@gmail.com>
2023-04-12 18:45:56 -0600

    Add cloudinary function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,21 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 //App initialization
 const app = express();
-
+dotenv.config();
 //Import routes
 import { postRoutes } from "./routes/postRoutes";
-
-//Route Middleware
-app.use("/api/posts", postRoutes);
 
 // MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+//ROUTES
+app.use("/api/posts", postRoutes);
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
