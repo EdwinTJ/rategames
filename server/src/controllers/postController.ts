e57084e1f73e2d20481f7e600260b16bb9df1429
EdwinTJ <edwin16x@gmail.com>
2023-04-19 19:47:05 -0600

    Deploy config

--- server/src/controllers/postController.ts
+++ server/src/controllers/postController.ts
@@ -1,4 +1,4 @@
-import { Example, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { cloudinary } from "../util/cloudinary";
 import HttpError from "../middleware/http-error";
 //import Types
